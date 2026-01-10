66d304f15dbbe8f494f291f555312df524183bf9
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-10 12:25:57 +0800

    Added hello person

---
+++ HelloPerson.java
@@ -0,0 +1,27 @@
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Methods
+    void displayInfo() {
+        System.out.println("Name: " + name + "\n" + "Age: " + age);
+    }
+}
+
+public class HelloPerson {
+    public static void main(String[] args) {
+        
+        // Creating an object
+        Person person1 = new Person("Bob", 14);
+
+        // Displaying the output
+        person1.displayInfo();
+
+    }
+}
