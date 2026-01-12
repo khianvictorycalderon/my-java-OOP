ac43fee8cfc58ab6369607349f663a8debc47a0c
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-12 21:09:06 +0800

    Modified class void

--- Abstraction.java
+++ Abstraction.java
@@ -1,8 +1,8 @@
 abstract class Animal {
     abstract void sound(); // Method must be implemented by subclass
 
-    void bark() {
-        System.out.println("Arrf Arrf!....");
+    void sleep() {
+        System.out.println("Sleeping...");
     }
 }
 
