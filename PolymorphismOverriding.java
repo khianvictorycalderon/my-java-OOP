b1d064cc28e8c74d1205c3e2e5d0fcbf0b9f5871
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-12 21:04:28 +0800

    Completed polymorphism overriding

--- PolymorphismOverriding.java
+++ PolymorphismOverriding.java
@@ -1,5 +1,24 @@
+class Greet {
+    void greet() {
+        System.out.println("Hello there!");
+    }
+}
+
+class Hello extends Greet {
+    @Override
+    void greet() {
+        System.out.println("Hi there, Overrided!");
+    }
+}
+
 public class PolymorphismOverriding {
     public static void main(String[] args) {
-        
+
+        Greet greeting1 = new Greet();
+        Hello hello1 = new Hello();
+
+        greeting1.greet();
+        hello1.greet();
+
     }
 }
