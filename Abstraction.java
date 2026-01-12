a78732d9a622628e53100db09ac3938c92b86c40
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-12 21:08:37 +0800

    Added abstraction

---
+++ Abstraction.java
@@ -0,0 +1,13 @@
+abstract class Animal {
+    abstract void sound(); // Method must be implemented by subclass
+
+    void bark() {
+        System.out.println("Arrf Arrf!....");
+    }
+}
+
+public class Abstraction {
+    public static void main(String[] args) {
+
+    }
+}
