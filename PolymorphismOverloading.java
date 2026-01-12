d1335fae460029d33398367dd540cbb4cb95648d
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-12 21:01:09 +0800

    Completed polymorphism overloading

--- PolymorphismOverloading.java
+++ PolymorphismOverloading.java
@@ -1,5 +1,23 @@
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
+
 public class PolymorphismOverloading {
     public static void main(String[] args) {
-        
+
+        Calculator calculate = new Calculator();
+
+        // Adding 2 numbers
+        System.out.println(calculate.add(5,3));
+        System.out.println(calculate.multiply(4,5));
+
     }
 }
